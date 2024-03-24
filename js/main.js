9703069789a5799ca3b112f62e6361bc6c1c612e
sasha <sashazemzer@gmail.com>
2024-03-24 15:13:47 +0200

    some animation and js

--- js/main.js
+++ js/main.js
@@ -30,17 +30,55 @@ $('#up-button').on('click', () => {
   window.scrollTo({top: 0, behavior: "smooth"});
 })
 
-$('#explore-cases-button').on('click', () => {
-  window.scrollTo({top: $(".case-studies-header").position().top, behavior: "smooth"});
-})
+const calculateAnimationStep = () => {
+  if (window.scrollY === 0 && !window.matchMedia("(max-width: 600px)").matches) {
+    const footerHeight = $(".floating-footer-container").height();
+    const footerMargin = Number.parseInt($(".main-footer-section").css("margin-top").replace("px", ""));
+    return footerHeight - footerMargin
+  }
+  return 0;
+};
+
+const calculateDescriptionBlockBottomPosition = () => {
+  const $description = $("#description-section");
+  return $description.position().top + $description.outerHeight();
+};
+
+const calculateHeaderBottomPadding = () => {
+  const visibleChild = $('.floating-header-container > *').not((index, element) => {
+    return $(element).css('display') === 'none';
+  });
+  return Number.parseInt(visibleChild.css("padding-bottom").replace("px", ""));
+};
+
+function calculateCasesScrollPosition() {
+  const animationStep = calculateAnimationStep();
+  const descriptionBlockBottomPosition = calculateDescriptionBlockBottomPosition();
+  const headerBottomPadding = calculateHeaderBottomPadding();
+  const floatingHeaderHeight = $(".floating-header-container").height();
+  const descriptionBottomCountingAnimation = descriptionBlockBottomPosition - animationStep;
+  const headerSizeWithoutTopPadding = floatingHeaderHeight - headerBottomPadding;
+  return descriptionBottomCountingAnimation - headerSizeWithoutTopPadding;
+}
+
+function scrollToCases() {
+  const scrollPosition = calculateCasesScrollPosition();
+  window.scrollTo({top: scrollPosition, behavior: "smooth"});
+}
+
+$('#explore-cases-button').on('click', () => window.scrollTo({top: calculateCasesScrollPosition(), behavior: "smooth"}))
 
 const $window = $(window);
-const $footer = $(".floating-footer-container");
+const $floatingFooter = $(".floating-footer-container");
 const $nextAfterFooter = $(".description-container");
 
 let lastScrollY = window.scrollY;
 
-$window.scroll(() => {
+function animateFooterOnScroll() {
+  if (window.matchMedia("(max-width: 600px)").matches) {
+    return;
+  }
+
   if (window.scrollY === 0) {
     $nextAfterFooter.addClass('description-container-margin').removeClass('description-container-margin-scrolled');
   } else {
@@ -48,9 +86,46 @@ $window.scroll(() => {
   }
 
   if (window.scrollY === 0 || window.scrollY < lastScrollY) {
-    $footer.addClass('animate__slideInUp').removeClass('animate__slideOutDown');
+    $floatingFooter.addClass('animate__slideInUp').removeClass('animate__slideOutDown');
   } else {
-    $footer.addClass('animate__slideOutDown').removeClass('animate__slideInUp');
+    $floatingFooter.addClass('animate__slideOutDown').removeClass('animate__slideInUp');
   }
   lastScrollY = window.scrollY;
+}
+
+$window.scroll(() => {
+  animateFooterOnScroll();
+
+  const $visibleHeaderMenu = $('.floating-header-container > *')
+    .not((index, element) => $(element).css('display') === 'none')
+    .find(".menu-buttons")
+    .first();
+
+  const $home = $visibleHeaderMenu.children(".home").first();
+  const $work = $visibleHeaderMenu.children(".work").first();
+  const $contacts = $visibleHeaderMenu.children(".contacts").first();
+  if (window.scrollY < calculateCasesScrollPosition()) {
+    $home.addClass("selected-menu-button")
+    $work.removeClass("selected-menu-button")
+    $contacts.removeClass("selected-menu-button")
+  } else if (window.innerHeight + Math.round(window.scrollY) < document.body.offsetHeight) {
+    $home.removeClass("selected-menu-button")
+    $work.addClass("selected-menu-button")
+    $contacts.removeClass("selected-menu-button")
+  } else {
+    $home.removeClass("selected-menu-button")
+    $work.removeClass("selected-menu-button")
+    $contacts.addClass("selected-menu-button")
+  }
 })
+
+
+$(".menu-buttons > .home").on('click', () => {
+  window.scrollTo({top: 0, behavior: "smooth"})
+});
+$(".menu-buttons > .work").on('click', () => {
+  window.scrollTo({top: calculateCasesScrollPosition(), behavior: "smooth"})
+});
+$(".menu-buttons > .contacts").on('click', () => {
+  window.scrollTo({top: document.body.offsetHeight, behavior: "smooth"})
+});
