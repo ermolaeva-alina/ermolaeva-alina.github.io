5a168073563d7d52ae6169bfa25a63dab9150e99
sasha <sashazemzer@gmail.com>
2024-04-28 13:59:49 +0300

    fix: small fixes following mockups

--- js/copied-popup.js
+++ js/copied-popup.js
@@ -12,16 +12,27 @@ export async function showCopiedPopup() {
   }
 
   showed = true;
-  $copiedPopup.css('display', 'block').addClass('animate__fadeInUp').removeClass('animate__fadeOutDown');
+
+  const classes = getShowAndHideClasses();
+
+  $copiedPopup.css('display', 'block').addClass(classes.show).removeClass(classes.hide);
 
   setTimeout(() => {
-    $copiedPopup.addClass('animate__fadeOutDown').removeClass('animate__fadeInUp');
+    $copiedPopup.addClass(classes.hide).removeClass(classes.show);
   }, 3000); // Элемент начнет исчезать через 3 секунд
 
   $copiedPopup.on('animationend', () => {
-    if ($copiedPopup.hasClass('animate__fadeOutDown')) {
+    if ($copiedPopup.hasClass(classes.hide)) {
       $copiedPopup.css('display', 'none');
       showed = false;
     }
+    $copiedPopup.removeClass(classes.hide)
   });
+}
+
+const getShowAndHideClasses = () => {
+  if (window.matchMedia("(max-width: 600px)").matches) {
+    return {show: 'animate__fadeInDown', hide: 'animate__fadeOutUp'}
+  }
+  return {show: 'animate__fadeInUp', hide: 'animate__fadeOutDown'}
 }
