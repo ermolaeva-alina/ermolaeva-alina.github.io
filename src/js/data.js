ee8f21cd83d41ab405e49503d1349b7f9607be08
ermolaeva-alina <79974883+ermolaeva-alina@users.noreply.github.com>
2024-06-08 21:30:31 +0300

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -33,21 +33,20 @@ const data = {
         "B2B",
         "Qualitative research",
         "Usability testing",
-        "2022"
       ]
     },
     {
-      title: "Case 2",
-      description: "Description 2",
-      imageName: "case-1.png",
+      title: "Life in science | Educational platform",
+      description: "Life in Science is an educational platform for ITMO University students studying soft skills. It was created as an alternative to offline courses for students who cannot attend in-person classes.",
+      imageName: "case-2.png",
       link: "https://google.com",
       tags: [
         "Design system",
         "UI design",
-        "B2B",
+        "B2C",
         "Qualitative research",
         "Usability testing",
-        "2022"
+        "Mentoring"
       ]
     },
     {
