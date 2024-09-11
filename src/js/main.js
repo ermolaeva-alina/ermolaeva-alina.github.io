cfb4a6b57cbe6512eed9e77bebb477438452ee74
sasha <sashazemzer@gmail.com>
2024-09-11 20:33:26 +0300

    fix animations for touch screens

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,6 @@ import '../components/arrow.js'
 import './cases.js'
 import './listeners.js'
 import './animated-tags.js'
-import './mobile-hover.js'
 import { initFooter } from './scroll'
 
 initFooter();
